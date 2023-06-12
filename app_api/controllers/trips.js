75f0cdae747bb2beb513cc101c03c7a0dba53b2a
SiberianFenris <causey.ronald@gmail.com>
2023-06-12 00:00:08 -0400

    Setting up SPA admin page

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -20,6 +20,48 @@ const fetchTrips = async (req, res) => {
     }
 };
 
+const addTrip = async (req, res) => {
+    const newTrip = req.body;
+
+    if(!newTrip) {
+        // 400 BAD REQUEST because no trip sent
+        res.status(400).sent('No trip record found in body of request');
+        return;
+    }
+
+    try {
+        const savedTrip = await trips.create(newTrip);
+
+        //201 CREATED response with the trip -- we send it back because it'll have mongodb id
+        res.status(201).json(savedTrip);
+    } catch (e) {
+        // 400 BAD REQUEST because trip failed to create
+        res.status(400).json(e);
+    }
+};
+
+const updateTrip = async (req, res) => {
+    const tripCode = req.params.tripCode;
+    let trip = req.body;
+
+    trip = Object.assign(trip, {tripCode});
+
+    try {
+        const updatedTrip = await trips.findOneAndUpdate({'code': tripCode}, trip, {new: true});
+
+        if(updateTrip == null) {
+            res.status(404).send(`Trip not found ${tripCode}`);
+            return;
+        }
+
+        res.status(200).json(updatedTrip);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+};
+
 module.exports = {
-    fetchTrips
+    fetchTrips,
+    addTrip,
+    updateTrip
 };
--- app_api/routes/trips.js
+++ app_api/routes/trips.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/trips');
 
 router.get('/:tripCode?', controller.fetchTrips);
+router.post('/', controller.addTrip);
+router.put('/:tripCode', controller.updateTrip);
 
 module.exports = router;
